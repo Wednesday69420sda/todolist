4cdb3451d6e17fe740b62750306fee7e0fa7f782
Tse <toxiclightz@gmail.com>
2022-08-09 17:08:00 +0800

    first commit

---
+++ src/Repository/TodoItemRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TodoItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method TodoItem|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TodoItem|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TodoItem[]    findAll()
+ * @method TodoItem[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TodoItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TodoItem::class);
+    }
+
+    // /**
+    //  * @return TodoItem[] Returns an array of TodoItem objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+}
